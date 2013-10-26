f14f573f0508335f1b008c70195b2e1069e8eea4
Rick Steckles <r.steckles@ctidigital.com>
2013-10-26 14:00:45 +0100

    Initial commit with basic init script and upstart template

---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+class mailcatcher {
+  package { 'sqlite3':
+    ensure => present
+  }
+
+  package { 'libsqlite3-dev':
+    ensure => present
+  }
+
+  package { 'mailcatcher':
+    provider => gem,
+    ensure => present,
+    require => Package[sqlite3, libsqlite3-dev]
+  }
+
+  file { '/etc/init/mailcatcher.conf':
+    content => template('mailcatcher/upstart.conf.erb'),
+    alias => 'mailcatcher.conf'
+  }
+
+  service { 'mailcatcher':
+    ensure   => running,
+    provider => upstart,
+    hasstatus => true,
+    require => [ File['mailcatcher.conf'], Package[mailcatcher] ]
+  }
+}
